f31b2cd7ec8d62f7ed81a613006325990b153288
Brian Butterfield <brian@butter-field.net>
2012-10-06 13:37:04 -0600

    Initial commit.  Calculator Exercise

---
+++ iOS/Calculator/Calculator/CalculatorModel.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorModel.h
+//  Calculator
+//
+//  Created by Butterfied on 9/19/12.
+//  Copyright (c) 2012 Brian G. Butterfield. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorModel : NSObject
+
+- (void)pushOperand:(double)operand;
+- (double)performOperation:(NSString *)operation;
+@end
---
+++ iOS/Calculator/Calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Butterfied on 9/19/12.
+//  Copyright (c) 2012 Brian G. Butterfield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CalculatorAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CalculatorAppDelegate class]));
+    }
+}
